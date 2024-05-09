6b727ace24acffa38e563f8eb408118dd530aaea
Joshua861 <joshrl@proton.me>
2024-05-09 16:19:25 +0100

    ...

--- src/lib/Board.svelte
+++ src/lib/Board.svelte
@@ -1,5 +1,7 @@
-<script>
+<script lang="ts">
 	import { highContrast } from '$lib/settings';
+	import { get } from 'svelte/store';
+	import { pos } from '$lib/state';
 	import { fly } from 'svelte/transition';
 
 	export let board;
@@ -11,11 +13,15 @@
 			<div
 				id={`id-${rowIndex}-${columnIndex}`}
 				class="m-[2px] flex aspect-square justify-center rounded-xl
-        border border-bg1 text-center align-middle text-3xl transition-all sm:m-1 sm:text-4xl"
+        {JSON.stringify($pos) == JSON.stringify({ x: columnIndex, y: rowIndex })
+					? 'border-2 border-fg1'
+					: 'border border-bg1'}
+        text-center align-middle text-3xl transition-all sm:m-1 sm:text-4xl"
 				class:bg-green={piece.state == 'correct'}
 				class:bg-yellow={piece.state == 'present'}
 				class:bg-bg1={piece.state == 'wrong'}
 				class:!bg-blue={piece.state == 'present' && $highContrast == true}
+				on:click={() => pos.set({ x: columnIndex, y: get(pos).y })}
 			>
 				{#if piece.letter !== null}
 					<span transition:fly={{ y: 30 }} class="my-auto font-mono font-bold leading-4">
